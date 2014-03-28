2bd084b26de3c12dbdc7fe28bdcd6f2821f37a85
jorgesangarr <jorgesangarr@gmail.com>
2014-03-28 11:46:30 +0100

    remove omniauth

--- db/migrate/20140325171706_add_extra_info_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddExtraInfoToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :nickname, :string
-    add_column :users, :image, :string
-    add_column :users, :location, :string
-    add_column :users, :gender, :string
-  end
-end
