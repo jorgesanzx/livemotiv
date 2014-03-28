720f2ecafde5fca4fca28e44564154801fe69950
jorgesangarr <jorgesangarr@gmail.com>
2014-03-28 19:08:17 +0100

    use devise only with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,32 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable,
+  devise :rememberable, :trackable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_one :profile
+
+  def self.sign_in_or_sign_up(provider, uid, user_info)
+    Rails.logger.debug "*"*80
+    Rails.logger.debug provider
+    Rails.logger.debug uid
+    Rails.logger.debug user_info
+    Rails.logger.debug "*"*80
+
+    User.create!(provider: provider, uid: uid)
+
+    Profile.create(user_info)
+  #   unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+  #     user = User.create nickname: auth_hash["info"]["nickname"],
+  #                        email: auth_hash["info"]["email"],
+  #                        name: auth_hash["info"]["name"],
+  #                        image: auth_hash["info"]["image"],
+  #                        location: auth_hash["info"]["location"],
+  #                        gender: auth_hash["extra"]["raw_info"]["gender"]
+  #     auth = create user: user, provider: auth_hash["provider"], uid: auth_hash["uid"]
+  #   end
+  # auth
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,8 @@ ActiveRecord::Schema.define(version: 20140328100953) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",       default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -44,8 +40,5 @@ ActiveRecord::Schema.define(version: 20140328100953) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
